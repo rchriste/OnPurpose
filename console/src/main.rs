04a863df1ad9bed3fe4179a70bf8b4b3a3598a5e
rchriste <russ-christensen@live.com>
2024-01-21 14:35:40 -0800

    Load Database file on Windows
    When updating to the latest package of Surreal it turns out that the default file location of ~ changed so this hack restores the old behavior of just pointing to c:\

--- console/src/main.rs
+++ console/src/main.rs
@@ -29,7 +29,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let data_storage_join_handle = tokio::spawn(async move {
         data_storage_start_and_run(
             have_data_storage_layer_use_to_receive_rx,
-            "file://~/.on_purpose.db",
+            "file://c:/.on_purpose.db", //TODO: Get a default file location that works for both Linux and Windows
         )
         .await
     });
